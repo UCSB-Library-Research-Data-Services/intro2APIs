2513719d6236c2f8f0bfa881c89d27ec430a67ec
jairomelo <jairoantoniomelo@gmail.com>
2024-12-10 19:40:56 -0800

    debugging

--- .github/scripts/fetch-cats.js
+++ .github/scripts/fetch-cats.js
@@ -2,6 +2,8 @@ const https = require('https');
 const fs = require('fs');
 
 const API_KEY = process.env.CAT_API_KEY;
+console.log('API Key exists:', !!API_KEY);
+
 const OUTPUT_FILE = '_data/cats.json';
 
 const options = {
@@ -12,6 +14,8 @@ const options = {
   }
 };
 
+console.log('Making request to:', `https://${options.hostname}${options.path}`);
+
 https.get(options, (resp) => {
   let data = '';
 
@@ -20,18 +24,22 @@ https.get(options, (resp) => {
   });
 
   resp.on('end', () => {
+    console.log('Response received:', data.substring(0, 100) + '...');
+    
     // Create directory if it doesn't exist
     const dir = '_data';
     if (!fs.existsSync(dir)){
         fs.mkdirSync(dir);
+        console.log('Created _data directory');
     }
     
     // Save the cat data
     fs.writeFileSync(OUTPUT_FILE, data);
-    console.log('Cat data saved successfully!');
+    console.log('Cat data saved to:', OUTPUT_FILE);
+    console.log('File contents:', fs.readFileSync(OUTPUT_FILE, 'utf8').substring(0, 100) + '...');
   });
 
 }).on("error", (err) => {
-  console.error("Error: " + err.message);
+  console.error("Error making request:", err.message);
   process.exit(1);
 });
